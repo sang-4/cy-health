75222575245af0e9cc91e2511095bc7bc084c652
Allan <kipropsallan@gmail.com>
2022-11-16 20:35:35 +0300

    updated component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import "./home.scss" 
 import Sidebar from '../../components/sidebar/Sidebar'
 import Navbar from '../../components/navbar/Navbar'
+import Widget from '../../components/widgets/Widget'
 
 const home = () => {
   return (
     <div className='home'> 
-    <Sidebar />
-    <div className="homeContainer">
-      <Navbar />
-    </div>
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+        <div className="widgets">
+          <Widget />
+        </div>
+        </div>
     </div>
   )
 }
