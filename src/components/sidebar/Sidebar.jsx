22f6962a82fe18e4e02ba510c6b763fc82546ffd
Allan <kipropsallan@gmail.com>
2022-11-16 16:53:27 +0300

    Updated styling

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import DashboardIcon from '@mui/icons-material/Dashboard';
 import "./sidebar.scss"
 
 const sidebar = () => {
   return (
     <div className='sidebar'>
-      <div className='top'>Juu</div>
-      <div className='bottom'>Chini</div>
+      <div className='top'>
+        <div className="spanlogo">Cy-Health</div>
+      </div>
+      <hr />
+      <div className='bottom'>
+        <ul>
+          <p className="title">Main</p>
+          <li>
+            <DashboardIcon className='icon' />
+            <span>Dashboard</span>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
