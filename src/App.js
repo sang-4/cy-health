5286b63359477b4db6d2ff3fd03971eda9fe2643
Allan <kipropsallan@gmail.com>
2022-11-16 15:22:30 +0300

    added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+  // Route,
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Country from "./pages/countries/Country";
+import Signup from "./pages/signup/Signup";
+import Login from "./pages/login/Login";
+import Single from "./pages/single/Single";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home/>
+  },
+  {
+    path: "/countries",
+    element: <Country/>
+  },
+  {
+    path: "/single",
+    element: <Single/>
+  },
+  {
+    path: "/signup",
+    element: <Signup/>
+  },
+  {
+    path: "/login",
+    element: <Login/>
+  },
+]);
 
 function App() {
   return (
     <div className="app">
-     CyHealth
+        <RouterProvider router={router} />
     </div>
   );
 }
 
 export default App;
+ 
